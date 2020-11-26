7b5f9a915b09b1a26db5466a78b8b4eb41f95f44
SaskiaR <saskia.rohtla@gmail.com>
2020-11-26 11:10:14 +0200

    Box for posts

--- src/views/Newsfeed.vue
+++ src/views/Newsfeed.vue
@@ -1,8 +1,19 @@
-// this is a copy of Welcome.vue
 <template>
+  <div class="home">
   <div >
     <Brand />
-    <UserProfile msg="Welcome to newsfeed" />
+<!--    <UserProfile msg="Welcome to newsfeed" />-->
+  </div>
+  <div class="registerhello text-center">
+    <h1 class="main-heading display-3 pt-5 mb-5">{{ "Newsfeed" }}</h1>
+    <div class="create-user-div row justify-content-center mx-3">
+      <div class="col-lg-6 col-md-4 col-sm-6 user-creation-card p-3">
+        <div style="text-align: left">
+          Latest posts:
+        </div>
+    </div>
+  </div>
+  </div>
   </div>
 </template>
 
