afd092dfce28a0fb4b7d0d296044b8db0b64d7b8
SaskiaR <saskia.rohtla@gmail.com>
2020-11-26 10:03:20 +0200

    Beginning of newsfeed page

---
+++ src/views/Newsfeed.vue
@@ -0,0 +1,23 @@
+// this is a copy of Welcome.vue
+<template>
+  <div >
+    <Brand />
+    <UserProfile msg="Welcome to newsfeed" />
+  </div>
+</template>
+
+<script>
+
+
+// @ is an alias to /src
+import RegisterSection from '@/components/RegisterSection.vue';
+import Brand from '@/components/Brand.vue';
+import UserProfile from "@/components/UserProfile.vue";
+
+export default {
+  name: 'Newsfeed',
+  components: {
+    Brand
+  },
+};
+</script>
