6549de3d368387b51109385844b4bc2d6403a39a
Ott Kask <ott.kask@gmail.com>
2020-11-26 09:26:18 +0200

    removed multiple duplicates of css

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,8 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
+  font-family: 'Poppins', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
