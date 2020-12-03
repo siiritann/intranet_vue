32be67346a67e287231bc6fafc3bdc8179c7e24c
Ott Kask <ott.kask@gmail.com>
2020-12-03 12:10:04 +0200

    file upload

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
     <h1>Intranet test</h1>
+    <FileUploader/>
   </div>
 </template>
+<script>
+import FileUploader from "@/components/FileUploader.vue";
+export default {
+  name: 'About',
+  components: {
+    FileUploader,
+  },
+};
+</script>
