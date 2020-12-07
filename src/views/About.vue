63c408e38fe33b30aed251a7f3b7fd4d7dca8c24
Ott Kask <ott.kask@gmail.com>
2020-12-07 13:14:05 +0200

    little update

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,11 @@
 <template>
   <div class="about">
     <h1>Intranet test</h1>
-    <ImageTool/>
-    
   </div>
 </template>
 <script>
-import ImageTool from "@/components/ImageTool.vue";
 export default {
   name: 'About',
-  components: {
-    ImageTool,
-  },
+  components: {},
 };
 </script>
