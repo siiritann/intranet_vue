0b1ff76f190c31eca1f533b5a4772bd099641afa
Ott Kask <ott.kask@gmail.com>
2020-12-10 14:30:32 +0200

    footer pluss deleted old files

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="registerhello text-center mb-3">
     <div>
-      <Brand/>
-      <Navbar/>
-      <LoginModal/>
-      <RegisterModal/>
+      <Brand />
+      <Navbar />
+      <LoginModal />
+      <RegisterModal />
     </div>
     <div class="home">
       <h1 class="main-heading display-3 pt-5 mb-5">Admin center</h1>
@@ -35,9 +35,10 @@
                 <td>
                   <div>
                     <button
-                        v-on:click="deleteUser(index)"
-                        type="button"
-                        class="redButton btn btn-outline-secondary btn-danger text-white btn-sm mt-1">
+                      v-on:click="deleteUser(index)"
+                      type="button"
+                      class="redButton btn btn-outline-secondary btn-danger text-white btn-sm mt-1"
+                    >
                       Delete
                     </button>
                   </div>
@@ -45,9 +46,10 @@
                 <td>
                   <div>
                     <button
-                        v-on:click="makeAdmin(index)"
-                        type="button"
-                        class="btn btn-outline-warning btn-sm yellowButton mt-1">
+                      v-on:click="makeAdmin(index)"
+                      type="button"
+                      class="btn btn-outline-warning btn-sm yellowButton mt-1"
+                    >
                       Make admin
                     </button>
                   </div>
@@ -60,16 +62,23 @@
             <div v-if="errorMessage" class="mx-1 py-2 alert alert-danger">
               Something went wrong.
             </div>
-            <div v-if="adminSuccessMessage" class="mx-1 py-2 alert alert-success">
+            <div
+              v-if="adminSuccessMessage"
+              class="mx-1 py-2 alert alert-success"
+            >
               User successfully marked as admin.
             </div>
-            <div v-if="deleteSuccessMessage" class="mx-1 py-2 alert alert-success">
+            <div
+              v-if="deleteSuccessMessage"
+              class="mx-1 py-2 alert alert-success"
+            >
               User successfully deleted.
             </div>
           </div>
         </div>
       </div>
     </div>
+    <Footer />
   </div>
 </template>
 
@@ -80,32 +89,33 @@ import Brand from '@/components/Brand.vue';
 import Navbar from '@/components/Navbar.vue';
 import LoginModal from '@/components/LoginModal.vue';
 import RegisterModal from '@/components/RegisterModal.vue';
-import router from "@/router";
-import jwt_decode from "jwt-decode";
+import router from '@/router';
+import jwt_decode from 'jwt-decode';
+import Footer from '@/components/Footer.vue';
 
 function getAllUsers() {
   let url = this.$server + '/user/list';
-  this.$http.get(url)
-      .then(response => {
-        this.usersList = response.data;
-      })
+  this.$http.get(url).then((response) => {
+    this.usersList = response.data;
+  });
 }
 
 function deleteUser(index) {
   let id = this.usersList[index].id;
   let url = this.$server + '/user/delete/' + id;
-  this.$http.delete(url)
-      .then(result => {
-        this.adminSuccessMessage = false;
-        this.deleteSuccessMessage = true;
-        this.errorMessage = false;
-        localStorage.removeItem('user-token');
-      })
-      .catch(error => {
-        this.adminSuccessMessage = false;
-        this.deleteSuccessMessage = false;
-        this.errorMessage = true;
-      });
+  this.$http
+    .delete(url)
+    .then((result) => {
+      this.adminSuccessMessage = false;
+      this.deleteSuccessMessage = true;
+      this.errorMessage = false;
+      localStorage.removeItem('user-token');
+    })
+    .catch((error) => {
+      this.adminSuccessMessage = false;
+      this.deleteSuccessMessage = false;
+      this.errorMessage = true;
+    });
   this.usersList.splice(index, 1);
 }
 
@@ -115,17 +125,18 @@ function makeAdmin(index) {
   let adminId = jwt_decode(token).intranetuserId;
   let userId = this.usersList[index].id;
   let url = this.$server + '/user/updaterole/' + adminId + '/' + userId;
-  this.$http.post(url)
-      .then(result => {
-        this.adminSuccessMessage = true;
-        this.deleteSuccessMessage = false;
-        this.errorMessage = false;
-      })
-      .catch(error => {
-        this.adminSuccessMessage = false;
-        this.deleteSuccessMessage = false;
-        this.errorMessage = true;
-      });
+  this.$http
+    .post(url)
+    .then((result) => {
+      this.adminSuccessMessage = true;
+      this.deleteSuccessMessage = false;
+      this.errorMessage = false;
+    })
+    .catch((error) => {
+      this.adminSuccessMessage = false;
+      this.deleteSuccessMessage = false;
+      this.errorMessage = true;
+    });
 }
 
 export default {
@@ -135,31 +146,32 @@ export default {
     Navbar,
     LoginModal,
     RegisterModal,
+    Footer,
   },
-  data: function () {
+  data: function() {
     return {
       usersList: [],
       adminSuccessMessage: false,
       deleteSuccessMessage: false,
       errorMessage: false,
-    }
+    };
   },
   methods: {
     getAllUsers,
     deleteUser,
-    makeAdmin
+    makeAdmin,
   },
   props: {
     token,
   },
-  mounted: function () {
+  mounted: function() {
     this.getAllUsers();
   },
 };
 </script>
 <style>
 .userslist {
-  margin-bottom: 80vh;
+  margin-bottom: 70vh;
 }
 .redButton {
   background-color: #d90707;
